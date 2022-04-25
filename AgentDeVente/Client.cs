7d8eaf3637cc7bc3191197b6fe3fdceed476c625
lemmouchimds <lemmouchimds@gmail.com>
2022-04-25 15:41:24 +0100

    Add Product and Client

---
+++ AgentDeVente/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLMainProject
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string Designation { get; set; }
+        public string Adress { get; set; }
+        public string Email { get; set; }
+        public bool IsRevendeur { get; set; }
+    }
+}
---
+++ AgentDeVente/Produit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLMainProject
+{
+    public class Produit
+    {
+        public int ID { get; set; } 
+        public string Designation { get; set; }
+        public float ValNutritionnelle { get; set; }
+        public float PoidsNet { get; set; }
+
+    }
+
+
+}
